616cd20b5b600d857c79503c8e528104549891e6
Zero2991 <zero2991@gmail.com>
2023-03-16 14:47:13 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description:with proper grammar, but the outcome is a piece of art, followed by a new line. 
+ * main - Entry point 
  * Return: Always 0 (Success)
  */
 int main(void)
