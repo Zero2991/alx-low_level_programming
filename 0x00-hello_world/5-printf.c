bd8f876aca562e7253643731b85af8a83942855c
Zero2991 <zero2991@gmail.com>
2023-03-16 13:53:40 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /*
- * main : is the main fucntion for this programm
+ * main	- is the main fucntion for this programm
  * return : always 0
  */
 int main(void)
-{	
-printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
-
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
+	 return (0);
 }
