a3817409d991225a6824997e03ccf069b5a6c191
Zero2991 <zero2991@gmail.com>
2023-03-16 13:31:06 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main - main fucntion of this program
+ * Return: always end with 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
