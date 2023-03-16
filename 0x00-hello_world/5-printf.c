65e29b88c4e08713135cd6db30e4ae8114364ef6
Zero2991 <zero2991@gmail.com>
2023-03-16 14:31:07 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints with proper grammar, but the outcome is a piece of art,
- * return - always 0
+ * main - Entry point
+ * 
+ * Description: 'with proper grammar, but the outcome is a piece of art,, followed by a new line.
+ * 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
