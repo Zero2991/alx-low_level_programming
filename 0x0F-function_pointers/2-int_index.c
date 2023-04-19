3f1a69004ad28d97ff90af902c1919b4cd740f04
Zero2991 <zero2991@gmail.com>
2023-04-19 10:07:50 -0700

    task2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+  * int_index - ...
+  * @array: ...
+  * @size: ...
+  * @cmp: ...
+  *
+  * Return: ...
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int j = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (j < size)
+			{
+				if (cmp(array[j]))
+					return (j);
+
+				j++;
+			}
+		}
+	}
+
+	return (-1);
+}
